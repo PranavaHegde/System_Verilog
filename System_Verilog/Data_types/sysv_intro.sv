2a7eb81a50a86e76ddbf89ecc80a5af2ae9032d7
PRANAVA HEGDE <pranuhegde23@gmail.com>
2026-05-26 20:35:35 +0530

    First

--- System_Verilog/Data_types/sysv_intro.sv
+++ System_Verilog/Data_types/sysv_intro.sv
@@ -1,2 +1,17 @@
-module sysv_intro();
-endmodule 
+module ex1;
+
+parameter SIZE=64;
+reg [SIZE-1:0] data;
+
+initial begin 
+  data = '0;
+  $display ("%b",data);
+  data = 'z;
+  $display ("%b",data);
+  data = 'x;
+  $display ("%b",data);
+  data = '1;  //'10;
+  $display ("%b",data);
+end 
+endmodule  
+ 
